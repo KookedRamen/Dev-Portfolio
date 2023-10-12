bce1be7b12dce9a496b5ad1940d6f2db91ebaf2a
Travis <travis.thundershield@gmail.com>
2023-10-12 09:29:27 -0400

    linked pages and styled navbars

--- contact.js
+++ contact.js
@@ -4,8 +4,6 @@ form.addEventListener('submit', (event) => {
 	const name = form.querySelector('#name').value;
 	const email = form.querySelector('#email').value;
 	const message = form.querySelector('#message').value;
-	// Send the data to a server or an email using AJAX, fetch or other methods
-	// You can also add validation or other logic here
 	console.log(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
 	form.reset();
 	alert('Thank you for contacting me!');
