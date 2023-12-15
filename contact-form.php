93c514486d0ef0f7aab4109a51cc5f361d96c2fc
Travis Thundershield <114441097+KookedRamen@users.noreply.github.com>
2023-12-14 20:33:21 -0500

    Mobile design changes

--- contact-form.php
+++ contact-form.php
@@ -1,5 +1,25 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect form data
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
 
-$name = $_POST["name"];
-$email = $_POST["email"];
-$message = $_POST["message"];
+    // Set up email parameters
+    $to = "travis.thundershield@gmail.com";
+    $subject = "New Message from $name";
+    $headers = "From: $email";
+
+    // Compose the email message
+    $email_message = "Name: $name\n";
+    $email_message .= "Email: $email\n";
+    $email_message .= "Message:\n$message";
+
+    // Send the email
+    if (mail($to, $subject, $email_message, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Failed to send email. Please try again.";
+    }
+}
+?>
