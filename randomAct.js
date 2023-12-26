622c9ee9f29f783cc7b0a0435b88b75d539a2958
Travis Thundershield <114441097+KookedRamen@users.noreply.github.com>
2023-12-26 14:06:07 -0500

    removed outdated JS to be updated

--- randomAct.js
+++ randomAct.js
@@ -17,11 +17,11 @@ $(document).ready(function() {
     });
   }
 
-  function displayActivity(activity) {
-    var activityContainer = document.getElementById("activityContainer");
-    activityContainer.innerHTML = "<h2>Activity:</h2>";
-    activityContainer.innerHTML += "<p><strong>Name:</strong> " + activity.activity + "</p>";
-    activityContainer.innerHTML += "<p><strong>Type:</strong> " + activity.type + "</p>";
-    activityContainer.innerHTML += "<p><strong>Participants:</strong> " + activity.participants + "</p>";
+  function displayActivity(activities) {
+    var activity = document.getElementById("activity");
+    activity.innerHTML = "<h2>Activity:</h2>";
+    activity.innerHTML += "<p><strong></strong> " + activities.activity + "</p>";
+    activity.innerHTML += "<p><strong>Type:</strong> " + activities.type + "</p>";
+    activity.innerHTML += "<p><strong>Participants:</strong> " + activities.participants + "</p>";
     
   }
